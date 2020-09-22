de9193021958904bf9955c1ce1ee00e6a4ee4a4e
Tanni <giovanni.benveniste@gmail.com>
2020-09-22 19:01:47 +0200

    fixing responsiveness

--- src/Banner.js
+++ src/Banner.js
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
-// import Search from "./Search";
+import Search from "./Search";
 import { useHistory } from "react-router-dom";
 
 const Banner = () => {
   const history = useHistory();
-  const [showSearch, setShowSearch] = useState(false);
+  // const [showSearch, setShowSearch] = useState(false);
 
   return (
     <div className="banner">
-      {/* <div className="banner__search">
-        {showSearch && <Search />}
-        <Button
-          onClick={() => setShowSearch(!showSearch)}
-          className="banner__searchButton"
-          variant="outlined"
-        >
-          {showSearch ? "Hide" : "Search Dates"}
-        </Button>
-      </div> */}
       <div className="banner__info">
+        <Search />
         <h1>Riscopri l'Italia</h1>
         <h5>Viaggia in modo diverso, scopri i tesori nascosti vicino a te.</h5>
         <Button onClick={() => history.push("/search")} variant="outlined">
--- src/Search.js
+++ src/Search.js
@@ -1,37 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Search.css";
-import { DateRangePicker } from "react-date-range";
-import "react-date-range/dist/styles.css";
-import "react-date-range/dist/theme/default.css";
-import { Button } from "@material-ui/core";
-import PeopleIcon from "@material-ui/icons/People";
-import { useHistory } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Search = () => {
-  const history = useHistory();
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-
-  const selectionRange = {
-    startDate,
-    endDate,
-    key: "selection",
-  };
-
-  const handleSelect = (ranges) => {
-    setStartDate(ranges.selection.startDate);
-    setEndDate(ranges.selection.endDate);
-  };
-
   return (
     <div className="search">
-      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
-      <h2>
-        Number of guests
-        <PeopleIcon />
-      </h2>
-      <input min={0} defaultValue={2} type="number" />
-      <Button onClick={() => history.push('/search')}>Search AirBnb</Button>
+      <div className="search-container">
+        <form className="search__form">
+          <label for="where"></label>
+          <SearchIcon color="disabled" />
+          <input type="text" id="where" placeholder="Dove vuoi andare?" />
+        </form>
+      </div>
     </div>
   );
 };
